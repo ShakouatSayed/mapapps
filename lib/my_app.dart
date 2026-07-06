c832eb13b86f34669907746ab272bd560a8e2a4f
Shakouat Hossen <sayedckb61@gmail.com>
2026-07-06 22:38:42 +0600

    new update

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mapapps/screen/map_screen.dart';
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MapScreen(),
-    );
+    return MaterialApp(home: MapScreen());
   }
 }
