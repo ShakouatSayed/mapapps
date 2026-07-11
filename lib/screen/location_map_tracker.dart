67a1e95f7b3b5de965e064f7ff08210f40202226
Shakouat Hossen <sayedckb61@gmail.com>
2026-07-11 07:40:07 +0600

    add location map tracker

--- lib/screen/location_map_tracker.dart
+++ lib/screen/location_map_tracker.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -10,6 +12,14 @@ class LocationMapTracker extends StatefulWidget {
 class _LocationMapTrackerState extends State<LocationMapTracker> {
   // Controller for the Google Map Widget, used to animate the camera.
   GoogleMapController? _mapController;
+
+  // Timerr that fires every 10 seconds to fetch a fresh location.
+  Timer? _locationTime;
+
+  // keeps the full trail of visited points so the polyline can keep growing
+  final List<LatLng> _routePoints = [];
+
+  //
   @override
   Widget build(BuildContext context) {
     return Scaffold();
